c8e6fc72dfa21fdb6e51aefb469f42d59213e998
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-04-01 22:55:07 -0500

    Added post processing

---
+++ Assets/Game/Scripts/TurnOffAfter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffAfter : MonoBehaviour
+{
+    public float turnOffAfter;
+
+    void OnEnable()
+    {
+        StartCoroutine(TurnOff());
+    }
+
+    IEnumerator TurnOff()
+    {
+        yield return new WaitForSeconds(turnOffAfter);
+        gameObject.SetActive(false);
+    }
+}
